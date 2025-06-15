73334233ba3dde17d6ddeb1b8996f320b4bbf680
Spisila <gabrielspisila1@gmail.com>
2025-06-15 13:16:55 -0300

    Tudo

---
+++ j.js
@@ -0,0 +1,29 @@
+const project_1_button = document.getElementById("to_project_1")
+project_1_button.addEventListener('click', goto_project_1)
+
+function goto_project_1() 
+{
+
+  window.location.href = "project1.html"
+
+}
+
+const project_2_button = document.getElementById("to_project_2")
+project_2_button.addEventListener('click', goto_project_2)
+
+function goto_project_2() 
+{
+
+  window.location.href = "project2.html"
+
+}
+
+const project_3_button = document.getElementById("to_project_3")
+project_3_button.addEventListener('click', goto_project_3)
+
+function goto_project_3() 
+{
+
+  window.location.href = "project3.html"
+
+}
---
+++ project_page.js
@@ -0,0 +1,8 @@
+const home_button = document.getElementById("home_page_button")
+home_button.addEventListener('click', goto_home_page)
+
+function goto_home_page() {
+
+  window.location.href = "s.html"
+
+}
